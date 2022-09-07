d939662bc0e545b7cc279e14e96eafdb8e30efe3
iLBigga <Giannanti1996@gmail.com>
2022-09-07 17:45:38 +0200

    Aggiunto axios

---
+++ src/components/CardsComponent.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "CardsComponent",
+    
+}
+</script>
+
+
+<style lang="scss" scoped>
+    
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,51 @@
+<template>
+    <main class="main-section">
+        <div class="container">
+            <div class="row">
+                <div 
+                    v-for="(album, i) in albumList"
+                    :key="i"
+                >
+                    <CardsComponent/>
+                </div>
+            </div>
+        </div>        
+    </main>
+</template>
+
+
+<script>
+    import axios from "axios";
+    import CardsComponent from './CardsComponent.vue';
+
+export default {
+    name: "MainComponent",
+    components: {
+        CardsComponent,
+    },
+    data(){
+        return {
+            albumList: []
+        }
+    },
+    created() {
+        axios
+            .get("https://flynn.boolean.careers/exercises/api/array/music")
+            .then((res) => {
+                this.albumList = res.data;
+            });    
+    },
+};
+</script>
+
+
+<style lang="scss" scoped>
+    @import "../styles/variables";
+
+    .main-section{
+        background-color: $dark-blue;
+        height: 100vh;
+        widows: 100vw;
+    }
+    
+</style>
